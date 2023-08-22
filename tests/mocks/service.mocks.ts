39555472a706292372b72600beb129ad96f15184
Samuel <omilosamuel@gmail.com>
2023-08-22 12:11:42 +0100

    chore: add unit tests for controller auth user

--- tests/mocks/service.mocks.ts
+++ tests/mocks/service.mocks.ts
@@ -1,12 +1,19 @@
-import { IUserService } from "src/utils/interfaces/services.interfaces";
-import { sampleUser } from "./user.repo";
+import { IUserService } from "src/utils/interfaces/services.interfaces"
+import { sampleUser } from "./user.repo"
 
 export const mockUserService: IUserService = {
-  createUser() {
-    return Promise.resolve(sampleUser);
-  },
+    createUser() {
+        return Promise.resolve(sampleUser)
+    },
 
-  getUsers() {
-    return Promise.resolve([sampleUser]);
-  },
-};
+    auth() {
+        return Promise.resolve({
+            user: sampleUser,
+            accessToken: "random-string",
+        })
+    },
+
+    getUsers() {
+        return Promise.resolve([sampleUser])
+    },
+}
--- tests/unit/controllers/user.controller.test.ts
+++ tests/unit/controllers/user.controller.test.ts
@@ -1,33 +1,56 @@
-import Container from "typedi";
-import UserController from "../../../src/controllers/user.controller";
-import { mockNext, mockReq, mockRes } from "../../mocks/utils.mock";
-import { mockUserService } from "../../../tests/mocks/service.mocks";
+import Container from "typedi"
+import UserController from "../../../src/controllers/user.controller"
+import { mockReq, mockRes } from "../../mocks/utils.mock"
+import { mockUserService } from "../../../tests/mocks/service.mocks"
+import { Request } from "express"
 
-describe('User controller', () => {
-    Container.set({ id: 'user_service', value: mockUserService})
+describe("User controller", () => {
+    Container.set({ id: "user_service", value: mockUserService })
     const userController = Container.get(UserController)
 
     beforeEach(() => {
         jest.clearAllMocks()
     })
-    
-    test('Controller calls user service to create user', async () => {
-        const createSpy = jest.spyOn(mockUserService, 'createUser')
 
-        await userController.createUser(mockReq, mockRes, mockNext)
+    test("Controller calls user service to create user", async () => {
+        const createSpy = jest.spyOn(mockUserService, "createUser")
+
+        await userController.createUser(mockReq, mockRes)
         expect(createSpy).toHaveBeenCalledTimes(1)
-        expect(mockRes.send).toHaveBeenCalledWith(expect.objectContaining({
-            message: 'User created'
-        }))
+        expect(mockRes.send).toHaveBeenCalledWith(
+            expect.objectContaining({
+                message: "User created",
+            })
+        )
     })
 
-    test('Controller calls get users', async () => {
-        const getSpy = jest.spyOn(mockUserService, 'getUsers')
+    test("Controller calls get users", async () => {
+        const getSpy = jest.spyOn(mockUserService, "getUsers")
 
-        await userController.getAllUsers(mockReq, mockRes, mockNext)
+        await userController.getAllUsers(mockReq, mockRes)
         expect(getSpy).toHaveBeenCalledTimes(1)
-        expect(mockRes.send).toHaveBeenCalledWith(expect.objectContaining({
-            message: 'Users'
-        }))
+        expect(mockRes.send).toHaveBeenCalledWith(
+            expect.objectContaining({
+                message: "Users",
+            })
+        )
+    })
+
+    test("Controller auth calls auth user service", async () => {
+        const authSpy = jest.spyOn(mockUserService, "auth")
+        const req = {
+            ...mockReq,
+            body: {
+                email: "sample",
+                password: "hey",
+            },
+        } as Request
+        await userController.authUser(req, mockRes)
+        expect(authSpy).toHaveBeenCalledTimes(1)
+        expect(mockRes.send).toHaveBeenCalledWith(
+            expect.objectContaining({
+                message: "User auth successful",
+            })
+        )
     })
 })
