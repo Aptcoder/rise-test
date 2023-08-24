7e6d0d7932f641c0ab4e24a3932ef5beb6b71cb3
Samuel <omilosamuel@gmail.com>
2023-08-24 16:36:05 +0100

    chore: update config for prod

--- config/production.ts
+++ config/production.ts
@@ -8,9 +8,9 @@ export default {
     database: {
         host: process.env.DB_HOST,
         port: 5432,
-        username: "samuel",
+        username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-        database: "rise",
+        database: process.env.DB_NAME,
         ssl: true,
     },
     redis: {
